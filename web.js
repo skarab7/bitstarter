ffeb52e681f2121a07d302d7a5873e24d3e22719
sk <sk>
2013-07-11 23:18:37 +0000

    Hello world from index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs  = require("fs");
 
 var app = express.createServer(express.logger());
 
+
+var data = fs.readFileSync("index.html");
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
