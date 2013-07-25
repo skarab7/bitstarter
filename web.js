c8ac1039ea0a1a22e5e427f2ba52c11e704dd8f3
wb <wb>
2013-07-25 20:09:25 +0000

    Change the name of the tool + add the visualization image

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs  = require("fs");
 
 var app = express.createServer(express.logger());
-
+app.use(express.static(__dirname + '/public'));
 
 var data = fs.readFileSync("index.html");
 
